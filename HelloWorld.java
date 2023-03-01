6e2bb4ff573888398ae1e15d398a3d48a4515e10
c-lech <lech.christian@gmail.com>
2023-03-01 14:28:39 -0300

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+// Classic Hello World program
+// File "HelloWorld.java"
+
+class HelloWorld
+{
+    // Name of main (or at least one) class should match the file name
+    // Do not want to program in Java, need it to learn devops from start to end!  
+
+    public static void main(String args[])
+    {
+        System.out.println("Hello World! I'm Christian.");
+    }
+}
+
+// javac HelloWorld.java to compile: .java -> .class
+// java HelloWorld to run
